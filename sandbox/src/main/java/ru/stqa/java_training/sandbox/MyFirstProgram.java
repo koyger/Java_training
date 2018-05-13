1b8d76ba09cf206131147475663d4bd259d5463e
koyger <koyger@gmail.com>
2018-05-14 00:10:23 +0300

    В ходе лекции "Основы программирования" добавлены 2 класса - Square и Rectangle, а также в этих классах добавлены методы вычисления их площадей

--- sandbox/src/main/java/ru/stqa/java_training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_training/sandbox/MyFirstProgram.java
@@ -5,21 +5,16 @@ public class MyFirstProgram {
         System.out.println("");
         hello("WORLD");
         hello("русское имя");
-        double len = 5;
-        System.out.println("Площадь квадрата со стороной "+len+" = " +area(len));
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами "+a+" и " +b+" = " +area(a,b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной "+s.l+" = " +s.area());
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами "+r.a+" и " +r.b+" = " +r.area());
     }
     public static void hello(String somebody) {
         System.out.println("Hello "+ somebody +"! Java Training.");
     }
-    public static double area (double l){
-        return l * l;
-    }
-    public static double area (double a, double b){
-        return a * b ;
-    }
+
+
 
 
 }
