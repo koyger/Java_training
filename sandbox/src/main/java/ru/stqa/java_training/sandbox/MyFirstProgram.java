f06c25adf994087312f7b6b8ae3d03444afba686
koyger <koyger@gmail.com>
2018-05-13 17:42:34 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/java_training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_training/sandbox/MyFirstProgram.java
@@ -1,10 +1,10 @@
 package ru.stqa.java_training.sandbox;
 
-public class MyFirstProgram
-{
-public static void main(String[] args){
+public class MyFirstProgram {
 
-System.out.println("Hello world! Java Training.");}
+  public static void main(String[] args) {
+    System.out.println("Hello world! Java Training.");
+    }
 
 
 }
