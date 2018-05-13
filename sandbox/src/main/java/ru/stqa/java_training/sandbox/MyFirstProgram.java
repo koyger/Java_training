f3ec16fdcfcd632a73e544d52c8719be4e185906
koyger <koyger@gmail.com>
2018-05-13 22:30:49 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/java_training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_training/sandbox/MyFirstProgram.java
@@ -1,9 +1,24 @@
 package ru.stqa.java_training.sandbox;
-
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world! Java Training.");
+    public static void main(String[] args) {
+        System.out.println("");
+        hello("WORLD");
+        hello("русское имя");
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной "+len+" = " +area(len));
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами "+a+" и " +b+" = " +area(a,b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello "+ somebody +"! Java Training.");
+    }
+    public static double area (double l){
+        return l * l;
+    }
+    public static double area (double a, double b){
+        return a * b ;
     }
 
 
