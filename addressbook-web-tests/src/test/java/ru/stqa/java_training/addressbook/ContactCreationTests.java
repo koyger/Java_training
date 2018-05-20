07da43bb4ada7a9615eb859d672db0bbc5318714
koyger <koygerov.work@gmail.com>
2018-05-20 20:56:56 +0300

    Выделен в отдельные методы и параметризован процесс создания пользователя. Для будущего удобства заполнения полей создан класс ContactData, содержащий поля контакта.

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/ContactCreationTests.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
-public class ContactCreationForm {
+public class ContactCreationTests {
     FirefoxDriver wd;
     
     @BeforeMethod
@@ -36,7 +36,7 @@ public class ContactCreationForm {
     public void testContactCreation() {
 
         gotoNewContactPage();
-        fillContactForm();
+        fillContactForm(new ContactData("Petr", "Petrovich", "Petrov", "petrishchev", "Mr.", "Petrosoft", "23 Main Str., San-Diego, CA, USA", "+1 999 999 99 99", "petr@gmail.com", "petr@petrosoft.com", "petrov.petrosoft.com", "Notes For Petrov"));
         submitConactForm();
         returnToContactsPage();
     }
@@ -49,49 +49,43 @@ public class ContactCreationForm {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillContactForm() {
+    private void fillContactForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Petr");
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
         wd.findElement(By.name("middlename")).click();
         wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys("Petrovich");
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getSecondName());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Petrov");
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
         wd.findElement(By.name("nickname")).click();
         wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("petrishchev");
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
         wd.findElement(By.name("title")).click();
         wd.findElement(By.name("title")).clear();
-        wd.findElement(By.name("title")).sendKeys("Mr.");
+        wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
         wd.findElement(By.name("company")).click();
         wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys("Petrosoft");
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("23 Main Str., San-Diego, CA, USA");
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("+1 999 999 99 99");
+        wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("pe");
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("petr@gmail.com");
+        wd.findElement(By.name("email")).sendKeys(contactData.getFirstEmail());
         wd.findElement(By.name("email2")).click();
         wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys("petr");
-        wd.findElement(By.name("email2")).click();
-        wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys("petr@petrosoft.com");
+        wd.findElement(By.name("email2")).sendKeys(contactData.getSecondEmail());
         wd.findElement(By.name("homepage")).click();
         wd.findElement(By.name("homepage")).clear();
-        wd.findElement(By.name("homepage")).sendKeys("petrov.petrosoft.com");
+        wd.findElement(By.name("homepage")).sendKeys(contactData.getHomePage());
         wd.findElement(By.name("notes")).click();
         wd.findElement(By.name("notes")).clear();
-        wd.findElement(By.name("notes")).sendKeys("Notes For Petrov");
+        wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
     }
 
     private void gotoNewContactPage() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/ContactData.java
@@ -0,0 +1,79 @@
+package ru.stqa.java_training.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String secondName;
+    private final String lastName;
+    private final String nickName;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String phone;
+    private final String firstEmail;
+    private final String secondEmail;
+    private final String homePage;
+    private final String notes;
+
+    public ContactData(String firstName, String secondName, String lastName, String nickName, String title, String company, String address, String phone, String firstEmail, String secondEmail, String homePage, String notes) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.phone = phone;
+        this.firstEmail = firstEmail;
+        this.secondEmail = secondEmail;
+        this.homePage = homePage;
+        this.notes = notes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getFirstEmail() {
+        return firstEmail;
+    }
+
+    public String getSecondEmail() {
+        return secondEmail;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
