4d591a33a58bc2a2251bcceed7709161ef7d1b19
koyger <koygerov.work@gmail.com>
2018-06-06 21:32:51 +0300

    В песочнице создан класс Equation, дающий количество корней квадратного уравнения. Создан класс EquationTests, проверяющий все 6 вариантов. Условия класса Equation переработаны для улучшения читаемости.

---
+++ sandbox/src/test/java/ru/stqa/java_training/sandbox/EquationTests.java
@@ -0,0 +1,42 @@
+package ru.stqa.java_training.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+
+    @Test
+    public void test0(){
+        Equation e = new Equation(1,1,1);
+        Assert.assertEquals(e.rootNumber(), 0);
+    }
+
+    @Test
+    public void test1(){
+        Equation e = new Equation(1,2,1);
+        Assert.assertEquals(e.rootNumber(), 1);
+    }
+    @Test
+    public void test2(){
+        Equation e = new Equation(1,5,6);
+        Assert.assertEquals(e.rootNumber(), 2);
+    }
+    /* проверки вырожденных уравнений */
+    @Test
+    public void testLinear(){
+        Equation e = new Equation(0,1,1);
+        Assert.assertEquals(e.rootNumber(), 1);
+    }
+    @Test
+    public void testConstant(){
+        Equation e = new Equation(0,0,4);
+        Assert.assertEquals(e.rootNumber(), 0);
+    }
+
+    @Test
+    public void testVoid(){
+        Equation e = new Equation(0,0,0);
+        Assert.assertEquals(e.rootNumber(), -1);
+    }
+
+}
