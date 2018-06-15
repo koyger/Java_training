80ca22711bbd858fca535169dc7abb72bdd7f518
koyger <koygerov.work@gmail.com>
2018-06-15 22:05:31 +0300

    Реализована проверка и подготовка предусловий для тестов  создания и редактирования групп

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.java_training.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.java_training.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -9,6 +10,9 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("TestGroup1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/GroupModificationTests.java
@@ -5,14 +5,19 @@ import ru.stqa.java_training.addressbook.model.GroupData;
 
 public class GroupModificationTests extends TestBase {
 
-    @Test
-    public void testGroupModification() {
-        app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().selectGroup();
-        app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("TestGroup1", "Header10", "Footer10"));
-        app.getGroupHelper().submitGroupModification();
-
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("TestGroup1", "test2", "test3"));
 
     }
+
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup1", "Header10", "Footer10"));
+    app.getGroupHelper().submitGroupModification();
+
+
+  }
 }
