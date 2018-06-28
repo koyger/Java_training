b6bac26ee6697f50a8e6c0b887357ab1b75b492d
koyger <koygerov.work@gmail.com>
2018-06-28 20:09:25 +0300

    Добавляю тесты из песочницы, чтобы продолжить обучение на другом компьютере

---
+++ sandbox/src/test/java/ru/stqa/java_training/sandbox/PrimeTests.java
@@ -0,0 +1,26 @@
+package ru.stqa.java_training.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+  @Test
+  public void testPrimes(){
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+
+  }
+  @Test
+  public void testPrimesFast(){
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+
+  }
+  @Test
+  public void testNonPrimes(){
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+  @Test(enabled = false)
+  public  void  testPrimeLong (){
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
