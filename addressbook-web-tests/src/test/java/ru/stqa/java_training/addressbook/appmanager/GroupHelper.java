7fb26cd7408b35454b3e001b46d31e6a32f07cfa
koyger <koygerov.work@gmail.com>
2018-07-02 13:24:51 +0300

    Для создания, модификации и удаления групп реализованы проверки путем сравнения списков групп. В итоге группы сравниваются по именам, ID со странички не учитывается.

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/appmanager/GroupHelper.java
@@ -2,8 +2,11 @@ package ru.stqa.java_training.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.java_training.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase  {
 
@@ -34,8 +37,9 @@ public class GroupHelper extends HelperBase  {
         click(By.name("delete"));
     }
 
-    public void selectGroup() {
-        click(By.name("selected[]"));
+    public void selectGroup(int index)
+    {
+        wd.findElements(By.name("selected[]")).get(index).click();
     }
 
     public void initGroupModification() {
@@ -60,5 +64,17 @@ public class GroupHelper extends HelperBase  {
 
     public int getGroupCount() {
         return  wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements){
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            GroupData group = new GroupData(id, name, null, null);
+            groups.add(group);
+        }
+        return groups;
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/tests/GroupDeletionTests.java
@@ -4,9 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.java_training.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
-
-
 
     @Test
     public void testGroupDeletion() {
@@ -14,12 +14,16 @@ public class GroupDeletionTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("TestGroup1", "test2", "test3"));
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(0);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before - 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size()  - 1);
+        before.remove(0);
+        Assert.assertEquals(before, after);
+
+
     }
 
 
