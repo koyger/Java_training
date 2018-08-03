621cbd9b1d5633295db6011adb699e2af445242d
koyger <koygerov.work@gmail.com>
2018-08-03 21:00:59 +0300

    Добавлено кэширование списков групп

--- addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_training/addressbook/appmanager/GroupHelper.java
@@ -54,6 +54,7 @@ public class GroupHelper extends HelperBase  {
     public void create(GroupData group) {
         initGroupCreation();
         fillGroupForm(group);
+        groupCache = null;
         submitGroupCreation();
         returnToGroupPage();
     }
@@ -67,27 +68,35 @@ public class GroupHelper extends HelperBase  {
         return  wd.findElements(By.name("selected[]")).size();
     }
 
+    private Groups groupCache = null;
+
     public Groups allGroups() {
-        Groups groups = new Groups();
+        if ( groupCache != null ) {
+            return new Groups(groupCache);
+        }
+        groupCache  = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements){
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
-        return groups;
+        return new Groups(groupCache);
     }
 
     public void modify(GroupData group) {
         selectGroupById(group.getId());
         initGroupModification();
         fillGroupForm(group);
+        groupCache = null;
         submitGroupModification();
+
     }
 
     public void delete(GroupData group) {
         selectGroupById(group.getId());
         deleteSelectedGroups();
+        groupCache = null;
         returnToGroupPage();
     }
 }
